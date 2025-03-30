b7c6f5a22c5a8a0e860c3e4656ceb4e0ff2d5138
Andrew William Watson <aww@awwsmm.com>
2025-03-30 09:28:23 -0400

    1.2-2

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,58 @@
-enum TokenType {}
+use std::fmt::{Display, Formatter};
+
+enum TokenType {
+    ILLEGAL,
+    EOF,
+
+    // Identifiers + literals
+    IDENT, // add, foobar, x, y, ...
+    INT, // 1343456
+
+    // Operators
+    ASSIGN,
+    PLUS,
+
+    // Delimiters
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    // Keywords
+    FUNCTION,
+    LET,
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let string = match self {
+            TokenType::ILLEGAL => "ILLEGAL",
+            TokenType::EOF => "EOF",
+
+            TokenType::IDENT => "IDENT",
+            TokenType::INT => "INT",
+
+            TokenType::ASSIGN => "=",
+            TokenType::PLUS => "+",
+
+            TokenType::COMMA => ",",
+            TokenType::SEMICOLON => ";",
+
+            TokenType::LPAREN => "(",
+            TokenType::RPAREN => ")",
+            TokenType::LBRACE => "{",
+            TokenType::RBRACE => "}",
+
+            TokenType::FUNCTION => "FUNCTION",
+            TokenType::LET => "LET",
+        };
+
+        write!(f, "{}", string)
+    }
+}
 
 struct Token {
     token_type: TokenType,
