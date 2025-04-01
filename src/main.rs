3c08aac53f4bb96f179ebeb57318e3dcdf0dcdb9
Andrew William Watson <aww@awwsmm.com>
2025-03-31 21:19:09 -0400

    1.5-1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use std::io;
+use std::io::Write;
+
 mod token;
 mod lexer;
 mod repl;
 
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = io::stdout();
+
+    stdout.write("Hello! This is the Monkey programming language!\n".as_bytes()).unwrap();
+    stdout.write("Feel free to type in commands\n".as_bytes()).unwrap();
+
+    repl::start(&mut io::stdin().lock(), &mut stdout)
 }
--- src/repl.rs
+++ src/repl.rs
@@ -3,7 +3,7 @@ use std::io::{BufRead, Write};
 
 const PROMPT: &'static [u8] = ">> ".as_bytes();
 
-fn start(reader: &mut impl BufRead, writer: &mut impl Write) {
+pub(crate) fn start(reader: &mut impl BufRead, writer: &mut impl Write) {
     loop {
         writer.write(PROMPT).unwrap();
         writer.flush().unwrap();
