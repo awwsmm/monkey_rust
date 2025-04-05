c4b9940b1306b95c43586008e556c85ec710d4c6
Andrew William Watson <aww@awwsmm.com>
2025-04-05 11:32:29 +0100

    2.4-2

--- src/ast.rs
+++ src/ast.rs
@@ -5,3 +5,17 @@ trait Node {
 trait Statement: Node {}
 
 trait Expression: Node {}
+
+struct Program {
+    statements: Vec<Box<dyn Statement>>
+}
+
+impl Node for Program {
+    fn token_literal(&self) -> String {
+        if self.statements.len() > 0 {
+            self.statements.iter().next().map(|s| (*s).token_literal()).unwrap()
+        } else {
+            String::from("")
+        }
+    }
+}
