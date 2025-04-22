bcdb39a09ecd240271350f90c9fcd70d5844e122
Andrew William Watson <aww@awwsmm.com>
2025-04-21 21:03:54 -0400

    4.3-6

--- src/evaluator/builtins.rs
+++ src/evaluator/builtins.rs
@@ -1,4 +1,5 @@
-use crate::evaluator;
+use crate::object::ObjectLike;
+use crate::{evaluator, object};
 
 pub(crate) struct Builtin{}
 
@@ -6,7 +7,25 @@ impl Builtin {
     pub(crate) fn from(string: &str) -> Option<evaluator::object::BuiltinObj> {
         match string {
             "len" => Some(evaluator::object::BuiltinObj{
-                func: |x| evaluator::NULL.unwrap()
+                func: |args| {
+                    if args.len() != 1 {
+                        return object::ErrorObj::new(format!(
+                            "wrong number of arguments. got={}, want=1",
+                            args.len()
+                        )).unwrap()
+                    }
+
+                    match args.get(0) {
+                        Some(object::Object::StringObj(arg)) =>
+                            return object::Object::IntegerObj(object::IntegerObj{ value: arg.value.len() as i32 }),
+
+                        _ =>
+                            return object::ErrorObj::new(format!(
+                                "argument to `len` not supported, got {:?}",
+                                args.get(0).map(|x| x.object_type())
+                            )).unwrap()
+                    }
+                }
             }) ,
             _ => None,
         }
