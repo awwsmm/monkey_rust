25281a169e64870e12dc95d75eade469f12d9e3b
Andrew William Watson <aww@awwsmm.com>
2025-04-29 18:57:11 -0400

    4.4-20

--- src/evaluator/builtins.rs
+++ src/evaluator/builtins.rs
@@ -1,12 +1,12 @@
 use crate::object::ObjectLike;
-use crate::{evaluator, object};
+use crate::object;
 
 pub(crate) struct Builtin{}
 
 impl Builtin {
-    pub(crate) fn from(string: &str) -> Option<evaluator::object::BuiltinObj> {
+    pub(crate) fn from(string: &str) -> Option<object::BuiltinObj> {
         match string {
-            "len" => Some(evaluator::object::BuiltinObj{
+            "len" => Some(object::BuiltinObj{
                 func: |args| {
                     if args.len() != 1 {
                         return object::ErrorObj::new(format!(
@@ -16,6 +16,9 @@ impl Builtin {
                     }
 
                     match args.get(0) {
+                        Some(object::Object::ArrayObj(arg)) =>
+                            return object::Object::IntegerObj(object::IntegerObj{ value: arg.elements.len() as i32 }),
+
                         Some(object::Object::StringObj(arg)) =>
                             return object::Object::IntegerObj(object::IntegerObj{ value: arg.value.len() as i32 }),
 
