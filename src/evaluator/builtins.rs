d3b0b5c995ead39cd400d7ea1865ac57b04148f2
Andrew William Watson <aww@awwsmm.com>
2025-04-29 19:41:38 -0400

    4.4-24

--- src/evaluator/builtins.rs
+++ src/evaluator/builtins.rs
@@ -125,6 +125,36 @@ impl Builtin {
                 }
             }),
 
+            "push" => Some(object::BuiltinObj{
+                func: |args| {
+                    if args.len() != 2 {
+                        return object::ErrorObj::new(format!(
+                            "wrong number of arguments. got={}, want=2",
+                            args.len()
+                        )).unwrap()
+                    };
+
+                    if args.get(0).unwrap().object_type() != object::ObjectType::ArrayObj {
+                        return object::ErrorObj::new(format!(
+                            "argument to `push` must be ArrayObj, got {:?}",
+                            args.get(0).unwrap().object_type()
+                        )).unwrap()
+                    };
+
+                    let arr = match args.get(0) {
+                        Some(object::Object::ArrayObj(inner)) => inner,
+                        _ => panic!()
+                    };
+
+                    let length = arr.elements.len();
+
+                    let mut new_elements = vec![object::Object::NullObj(object::NullObj{}); length];
+                    new_elements.clone_from_slice(&arr.elements);
+                    new_elements.push(args.get(1).cloned().unwrap());
+                    object::Object::ArrayObj(object::ArrayObj{ elements: new_elements })
+                }
+            }),
+
             _ => None,
         }
     }
