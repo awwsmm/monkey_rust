bc6dcaef39598d5d54cd1b77be51ee86752fd961
Andrew William Watson <aww@awwsmm.com>
2025-05-05 12:08:41 -0400

    2.0-1

--- src/code.rs
+++ src/code.rs
@@ -1,3 +1,22 @@
 struct Instructions(Vec<u8>);
 
-enum Opcode {}
+enum Opcode {
+    OpConstant,
+}
+
+impl Into<u8> for Opcode {
+    fn into(self) -> u8 {
+        match self {
+            Opcode::OpConstant => 0,
+        }
+    }
+}
+
+impl Into<Opcode> for u8 {
+    fn into(self) -> Opcode {
+        match self {
+            0 => Opcode::OpConstant,
+            _ => panic!("unknown opcode discriminant: {}", self)
+        }
+    }
+}
