232f832f34edbcd5df31739bd899650339a83949
Andrew William Watson <aww@awwsmm.com>
2025-05-31 15:44:03 -0400

    7.1-21

--- src/vm/frame.rs
+++ src/vm/frame.rs
@@ -3,11 +3,16 @@ use crate::{code, object};
 pub(crate) struct Frame {
     func: object::CompiledFunctionObj,
     pub(crate) ip: i32,
+    base_pointer: i32,
 }
 
 impl Frame {
-    pub(crate) fn new(func: object::CompiledFunctionObj) -> Self {
-        Self { func, ip: -1 }
+    pub(crate) fn new(func: object::CompiledFunctionObj, base_pointer: i32) -> Self {
+        Self {
+            func,
+            ip: -1,
+            base_pointer,
+        }
     }
 
     pub(crate) fn instructions(&self) -> code::Instructions {
