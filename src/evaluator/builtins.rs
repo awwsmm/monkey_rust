22d1ec69622ea7369364be4bed8f14dfda5e99a5
Andrew William Watson <aww@awwsmm.com>
2025-06-08 21:12:36 -0400

    8.0-10

--- src/evaluator/builtins.rs
+++ src/evaluator/builtins.rs
@@ -5,20 +5,6 @@ pub(crate) struct Builtin{}
 
 impl Builtin {
     pub(crate) fn from(string: &str) -> Option<object::BuiltinObj> {
-        match string {
-            "len" => get_builtin_by_name(string).cloned(),
-
-            "first" => get_builtin_by_name(string).cloned(),
-
-            "last" => get_builtin_by_name(string).cloned(),
-
-            "rest" => get_builtin_by_name(string).cloned(),
-
-            "push" => get_builtin_by_name(string).cloned(),
-
-            "puts" => get_builtin_by_name(string).cloned(),
-
-            _ => None,
-        }
+        get_builtin_by_name(string).cloned()
     }
 }
